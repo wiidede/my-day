3e5a329ea3b3f3a18682829a85fcc243800accc0
wiidede <wiixdede@gmail.com>
2023-03-08 12:18:49 +0800

    feat: logo

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Fly My Day',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'A simple and delicate day planner' },
     {
       name: 'theme-color',
-      content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+      content: computed(() => isDark.value ? '#252526' : '#ffffff'),
     },
   ],
   link: [
