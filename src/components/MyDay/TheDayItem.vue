28369da9f6c04ad4d7c4e0e90d00db4661d789b4
wiidede <wiixdede@gmail.com>
2023-03-10 01:23:32 +0800

    feat: the day responsible

--- src/components/MyDay/TheDayItem.vue
+++ src/components/MyDay/TheDayItem.vue
@@ -8,7 +8,7 @@ defineProps<{
 
 <template>
   <div
-    class="p2 flex"
+    class="px4 py2 flex flex-col items-start md:flex-row md:px2"
     :style="{
       '--progress-value': `${progress || 0}%`,
       '--progress-ripple-scale': `${Math.max(5, (progress || 0) / 2)}`,
@@ -16,16 +16,16 @@ defineProps<{
       '--progress-ripple-delay': `${Math.max(1.25, (progress || 0) / 20)}s`,
     }"
     :class="{
-      'the-progress py4 text-xl my-bg-primary/15 my-shadow': progress !== undefined,
+      'the-progress !py4 text-xl my-bg-primary/15 my-shadow': progress !== undefined,
     }"
   >
     <div v-if="progress !== undefined" class="the-progress-thumb" />
-    <div class="flex-1 text-center">
+    <div class="md:flex-1 text-center">
       <slot name="time">
         {{ time }}
       </slot>
     </div>
-    <div class="flex-3">
+    <div class="md:flex-3">
       <slot name="content">
         {{ content }}
         <span v-if="progress !== undefined">({{ progress.toFixed(2) }}%)</span>
