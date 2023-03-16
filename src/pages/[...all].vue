56eea03eb0f25ea627a208ca585a74d261729e23
wiidede <wiixdede@gmail.com>
2023-03-16 23:01:55 +0800

    feat: 404

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 const { t } = useI18n()
+const router = useRouter()
 </script>
 
 <template>
-  <div>
+  <h1 class="text-4xl p6">
     {{ t('not-found') }}
+  </h1>
+  <div class="m8 flex items-center gap-8 justify-center">
+    <RouterLink class="my-icon-btn my-c-primary text-4xl p3" to="/" :title="t('button.home')">
+      <div i-the-my-day />
+    </RouterLink>
+    <button class="my-icon-btn my-c-primary" @click="router.back()">
+      <div class="p2 text-2xl flex items-center gap-2">
+        <div i-carbon-arrow-left /><span>{{ t('button.back') }}</span>
+      </div>
+    </button>
   </div>
 </template>
-
-<route lang="yaml">
-meta:
-  layout: 404
-</route>
